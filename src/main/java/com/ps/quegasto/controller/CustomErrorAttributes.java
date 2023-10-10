385fa2a1b7275ea6de6ae7c063113cc70cc0c01c
Esteban <avila344@gmail.com>
2023-10-10 00:17:48 -0300

    agrego pagina de error para la app

---
+++ src/main/java/com/ps/quegasto/controller/CustomErrorAttributes.java
@@ -0,0 +1,18 @@
+package com.ps.quegasto.controller;
+import java.util.Map;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        return errorAttributes;
+    }
+}
+
